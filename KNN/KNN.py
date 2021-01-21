f521542187c0c030a3e990b57415860dc9fa81c9
Lemon <lemon412@foxmail.com>
2021-01-22 01:33:14 +0800

    update

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -1,4 +1,6 @@
 import numpy as np
+from copy import deepcopy
+from random import randint
 
 
 class KNN:
@@ -15,6 +17,9 @@ class KNN:
         self.__train_x = train_x
         self.__train_y = train_y
 
+    def train(self):
+        return
+
     def inference(self, inference_x):
         """
         kNN预测函数
@@ -26,5 +31,64 @@ class KNN:
             tmp.append(np.array([np.linalg.norm(inference_x - self.__train_x[i]), self.__train_y[i]]))
         tmp = np.array(tmp)
         tmp = tmp[np.argsort(tmp[:, 0])][0:self.__k]  # 根据第一个元素排序取前k大
-        tmp = np.argmax(np.bincount(np.array(tmp.T[1], dtype=np.int32)))  # 找到频数最多的数
-        return tmp
+        ans = np.argmax(np.bincount(np.array(tmp.T[1], dtype=np.int32)))  # 找到频数最多的数
+        return ans
+
+
+class CondenseKNN:
+    def __init__(self, train_x, train_y, k):
+        """
+        Condense kNN初始化函数
+        :param train_x: numpy.array，训练集的特征
+        :param train_y: numpy.array，训练集的目标
+        :param k: int 预测时选取的临近样本个数
+        """
+        assert(len(train_x) == len(train_y))
+        self.__n = len(train_x)
+        self.__k = k
+        self.__raw_x = train_x
+        self.__raw_y = train_y
+        self.__store_x = None
+        self.__store_y = None
+        self.__grab_bag_x = None
+        self.__grab_bag_y = None
+
+    def train(self):
+        self.__store_x = []
+        self.__store_y = []
+        self.__grab_bag_x = deepcopy(self.__raw_x)
+        self.__grab_bag_y = deepcopy(self.__raw_y)
+        for _ in range(self.__k):
+            assert len(self.__grab_bag_x) == len(self.__grab_bag_y)
+            cur = randint(0, len(self.__grab_bag_x) - 1)
+            self.__store_x.append(self.__grab_bag_x[cur])
+            self.__store_y.append(self.__grab_bag_y[cur])
+            self.__grab_bag_x = np.delete(self.__grab_bag_x, cur, axis=0)
+            self.__grab_bag_y = np.delete(self.__grab_bag_y, cur)
+        while True:
+            knn = KNN(self.__store_x, self.__store_y, self.__k)
+            assert len(self.__grab_bag_x) == len(self.__grab_bag_y)
+            for i in range(len(self.__grab_bag_x)):
+                if knn.inference(self.__grab_bag_x[i]) != self.__grab_bag_y[i]:
+                    self.__store_x.append(self.__grab_bag_x[i])
+                    self.__store_y.append(self.__grab_bag_y[i])
+                    self.__grab_bag_x = np.delete(self.__grab_bag_x, i, axis=0)
+                    self.__grab_bag_y = np.delete(self.__grab_bag_y, i)
+                    break
+            else:
+                break
+        # print(f"{len(self.__raw_x)} -> {len(self.__store_x)}")
+
+    def inference(self, inference_x):
+        """
+        Condense kNN预测函数
+        :param inference_x: numpy.array，预测特征
+        :return: 预测结果
+        """
+        tmp = []
+        for i in range(len(self.__store_x)):  # 对每一个train计算曼哈顿距离
+            tmp.append(np.array([np.linalg.norm(inference_x - self.__store_x[i]), self.__store_y[i]]))
+        tmp = np.array(tmp)
+        tmp = tmp[np.argsort(tmp[:, 0])][0:self.__k]  # 根据第一个元素排序取前k大
+        ans = np.argmax(np.bincount(np.array(tmp.T[1], dtype=np.int32)))  # 找到频数最多的数
+        return ans
---
+++ KNN/genderClassify.py
@@ -0,0 +1,164 @@
+from KNN import KNN, CondenseKNN
+import numpy as np
+import pandas as pd
+from plotly.subplots import make_subplots
+from copy import deepcopy
+import plotly.graph_objects as go
+import sys
+from types import MethodType
+
+sys.path.append("..")
+from PCA.PCA import PCA
+
+
+def leave_one_out(x, y, k, condense=False):
+    """
+    使用留一法验证法对模型进行评估
+    :return: 模型留一法验证下的正确率
+    """
+    cnt = 0
+    for i in range(len(x)):
+        train_x = np.delete(x, i, 0)
+        train_y = np.delete(y, i, 0)
+        test_x = x[i]
+        test_y = y[i]
+        if condense:
+            knn = CondenseKNN(train_x, train_y, k)
+        else:
+            knn = KNN(train_x, train_y, k)
+        knn.train()
+        test_result = knn.inference(test_x)
+        if test_result == test_y:
+            cnt += 1
+    return cnt / len(x)
+
+
+def normalize(data):
+    """
+    将数据进行归一化处理
+    :param data: 待处理的数据，np.array
+    :return: 归一化后的数据
+    """
+    data = deepcopy(data).T
+    for i in range(len(data)):
+        _min, _max = np.min(data[i]), np.max(data[i])
+        data[i] = (data[i] - _min) / (_max - _min)
+    return deepcopy(data.T)
+
+
+def inference(self, inference_x):
+    """
+    对二分类问题重写KNN预测函数，得到可能性
+    """
+    tmp = []
+    for i in range(self._KNN__n):  # 对每一个train计算曼哈顿距离
+        tmp.append(np.array([np.linalg.norm(inference_x - self._KNN__train_x[i]), self._KNN__train_y[i]]))
+    tmp = np.array(tmp)
+    tmp = tmp[np.argsort(tmp[:, 0])][0:self._KNN__k]  # 根据第一个元素排序取前k大
+    return sum(tmp.T[1]) / self._KNN__k
+
+
+def main():
+    raw_data = pd.read_csv("student_data.csv")
+    x = np.array(raw_data.iloc[:, 1:])
+    y = np.array(np.array(raw_data).T[0].T, dtype=int)
+    fig = make_subplots(
+        rows=1, cols=2,
+        column_width=[0.5, 0.5],
+        specs=[
+            [{"type": "scatter"}, {"type": "scatter"}],
+        ]
+    )
+    accuracy = []
+    for k in range(1, 50, 2):
+        accuracy.append(leave_one_out(x, y, k))
+    fig.add_trace(
+        go.Scatter(
+            x=list(range(1, 50, 2)), y=accuracy,
+            name=f"raw data", mode="lines",
+        ),
+        row=1, col=1,
+    )
+    normalized_x = normalize(x)
+    accuracy = []
+    for k in range(1, 50, 2):
+        accuracy.append(leave_one_out(normalized_x, y, k))
+    fig.add_trace(
+        go.Scatter(
+            x=list(range(1, 50, 2)), y=accuracy,
+            name=f"normalized", mode="lines",
+        ),
+        row=1, col=1,
+    )
+    pca = PCA(x)
+    pca_x = np.array(pca.pca(features=2))
+    accuracy = []
+    for k in range(1, 50, 2):
+        accuracy.append(leave_one_out(pca_x, y, k))
+    fig.add_trace(
+        go.Scatter(
+            x=list(range(1, 50, 2)), y=accuracy,
+            name=f"pca", mode="lines",
+        ),
+        row=1, col=1,
+    )
+    accuracy = []
+    for k in range(1, 50, 2):
+        accuracy.append(leave_one_out(normalized_x, y, k, condense=True))
+    fig.add_trace(
+        go.Scatter(
+            x=list(range(1, 50, 2)), y=accuracy,
+            name=f"normalized + condense", mode="lines",
+        ),
+        row=1, col=1,
+    )
+    accuracy = []
+    for k in range(1, 50, 2):
+        accuracy.append(leave_one_out(pca_x, y, k, condense=True))
+    fig.add_trace(
+        go.Scatter(
+            x=list(range(1, 50, 2)), y=accuracy,
+            name=f"pca + condense", mode="lines",
+        ),
+        row=1, col=1,
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=pca_x[y == 0, 0], y=pca_x[y == 0, 1],
+            name=f"male", mode="markers", marker_symbol="square",
+        ),
+        row=1, col=2,
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=pca_x[y == 1, 0], y=pca_x[y == 1, 1],
+            name=f"female", mode="markers", marker_symbol="circle",
+        ),
+        row=1, col=2,
+    )
+    knn = KNN(pca_x, y, 15)
+    knn.inference = MethodType(inference, knn)  # 重写inference函数
+    xrange = np.arange(pca_x[:, 0].min() - 0.1, pca_x[:, 0].max() + 0.1, 0.01)
+    yrange = np.arange(pca_x[:, 1].min() - 0.1, pca_x[:, 1].max() + 0.1, 0.01)
+    print(f"xrange: {len(xrange)}, yrange: {len(yrange)}")
+    zz = []
+    for _x in xrange:
+        zz.append([])
+        for _y in yrange:
+            ret = knn.inference(np.array([_x, _y]))
+            zz[-1].append(ret)
+    zz = np.array(zz).T
+    fig.add_trace(
+        go.Contour(
+            x=xrange, y=yrange, z=zz,
+            showscale=False, colorscale='RdBu',
+            opacity=0.4, name='Score',
+        ),
+        row=1, col=2,
+    )
+    fig.show()
+
+
+if __name__ == '__main__':
+    main()
